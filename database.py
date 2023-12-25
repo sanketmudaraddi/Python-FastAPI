176732761aa81b787a3527e7f361976b7d6f1660
Sanket Mudaraddi <sanketsm15@gmail.com.com>
2023-12-25 13:19:04 +0530

    Initial commit

---
+++ database.py
@@ -0,0 +1,18 @@
+# database.py
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    completed = Column(Boolean, default=False)
+
+Base.metadata.create_all(bind=engine)
